9f482d675ccffe2d1dafa1d05e0e1ed39ac03de9
Mikhail Rohau <misha_rogov@tut.by>
2018-03-01 00:58:59 +0300

    Advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let sum = 0;
+    let i = 2;
+    let factorBase = base;
+    while (i < factorBase) {
+        if (factorBase % i === 0) {
+            factorBase /= i;
+            if (factorBase === i) {
+                factorBase = factorBase + i - 1;
+                break;
+            }
+        } else {
+            i++;
+        }
+    }
+    let factor = factorBase;
+    while (factor <= number) {
+        sum += Math.trunc(number / factor);
+        factor *= factorBase;
+    }
+
+    return sum;
 }
